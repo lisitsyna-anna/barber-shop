d2f26c98b6042d1af5d6b6b5f3a1018544435ef8
Anna Lisitsyna <anna.lisitsyna93@gmail.com>
2022-07-04 15:53:23 +0200

    add modal and tablet

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,37 @@
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector('[data-modal-open]'),
+//     closeModalBtn: document.querySelector('[data-modal-close]'),
+//     modal: document.querySelector('[data-modal]'),
+//     body: document.querySelector('body'),
+//   };
+
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
+
+//   function toggleModal() {
+//     refs.modal.classList.toggle('is-hidden');
+//     refs.body.classList.toggle('no-scroll');
+//   }
+// })();
+
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
+    openModalBtn: document.querySelectorAll('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
-    body: document.querySelector('body'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
+  console.log(refs.openModalBtn);
+
+  refs.openModalBtn.forEach(function (btn) {
+    btn.addEventListener('click', toggleModal);
+  });
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
+    // document.body.classList.toggle('no-scroll');
     refs.modal.classList.toggle('is-hidden');
-    refs.body.classList.toggle('no-scroll');
+    document.body.classList.toggle('modal-open');
   }
 })();
